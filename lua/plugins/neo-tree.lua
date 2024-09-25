9d812aca193c2126bffacddbc1c7b575790ffcf6
Daniel Grychtoł <grychtol.daniel@gmail.com>
2024-09-25 13:10:48 +0200

    feat: add telescope-file-browser, change keybindings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,6 +11,14 @@ return {
     {
       "<leader>e",
       function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+      remap = true,
+    },
+    {
+      "<leader>ee",
+      function()
         require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd(), position = "current" })
       end,
       desc = "Explorer NeoTree (root dir)",
